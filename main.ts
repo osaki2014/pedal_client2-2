628ae4d158a54fac166b385902e95559ff3306a6
osaki <osaki2014@users.noreply.github.com>
2020-07-29 14:54:00 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    offset = offset + 5
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(id)
+})
+radio.onReceivedString(function (receivedString) {
+    if (parseFloat(receivedString) == id) {
+        radio.sendNumber(result)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    offset = offset - 5
+})
+function _do () {
+    result = Math.idiv(value, 8)
+    if (result >= 5) {
+        result = 4
+    }
+    if (result <= -1) {
+        result = 0
+    }
+}
+let y = 0
+let value = 0
+let result = 0
+let offset = 0
+let id = 0
+radio.setGroup(99)
+id = 3
+offset = 0
+basic.forever(function () {
+    for (let index = 0; index <= 4; index++) {
+        led.unplot(index, result)
+    }
+    y = 0
+    value = input.rotation(Rotation.Pitch) - offset
+    _do()
+    for (let index = 0; index <= 4; index++) {
+        led.plot(index, result)
+    }
+})
